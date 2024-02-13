cfb3429f972a132adc55f3bba99908a607c635ef
Andrey Romashev <andreygaranin.0@yandex.ru>
2024-02-13 17:06:53 +0700

    add hover effect

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,23 @@
-// import CardsContainer from '../CardsContainer/CardsContainer';
+import CardsContainer from '../CardsContainer/CardsContainer';
+import { useState } from 'react';
 import Book from '../Book/Book';
 import Search from '../Search/Search';
 import './App.sass';
 
-
 const App = () => {
+
+  const [showBook, setShowBook] = useState<boolean>(false)
+
   return (
     <div className='app'>
       <Search />
-      <Book/> 
-      {/* <div className="found-results">Found 447 results</div>
-      <CardsContainer/> */}
-
+      {showBook ?
+        <Book /> :
+        <>
+          <div className="found-results">Found 447 results</div>
+          <CardsContainer />
+        </>
+      }
     </div>
   )
 }
