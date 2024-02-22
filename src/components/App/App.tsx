90ff6a5a97128c2553a2a349eb00b55bf1fc5d0d
Andrey Romashev <andreygaranin.0@yandex.ru>
2024-02-22 13:23:48 +0700

    add google books api

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,36 @@
 import CardsContainer from '../CardsContainer/CardsContainer';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Book from '../Book/Book';
 import Search from '../Search/Search';
 import './App.sass';
+import { IBook } from '../../types/types';
 
 const App = () => {
 
   const [showBook, setShowBook] = useState<boolean>(false)
+  const [booksData, setBooksData] = useState<IBook>(
+    {
+      'category': 'Computers',
+      'cover': 'https://clck.ru/38o4PH',
+      'title': 'Node.js Разработка серверных веб- приложений на JavaScript',
+      'author': 'Дэвид Хэррон',
+      'discription': 'описание книги'
+    }
+  )
+  const [books, setBooks] = useState<Array<any>>([])
+
+  useEffect(() => {
+    fetch('https://www.googleapis.com/books/v1/volumes?q=JavaScript&maxResults=30', {
+      headers: {
+        'Authorization': 'key=AIzaSyC9Ujvz5Ff7wxHAm8IPmU0xf634nieW8Qk',
+      }
+    })
+      .then(res => res.json())
+      .then((data) => {
+        // console.log(data.items);
+        setBooks(data.items)
+      });
+  }, []);
 
   return (
     <div className='app'>
@@ -15,6 +39,8 @@ const App = () => {
         <Book /> :
         <>
           <div className="found-results">Found 447 results</div>
+          {books[0] !== undefined && console.log(books[0].volumeInfo.title)}
+          {/* <div className="found-results">{console.log(books)}</div> */}
           <CardsContainer />
         </>
       }
---
+++ src/types/types.ts
@@ -0,0 +1,7 @@
+export interface IBook {
+  'category': string,
+  'cover': string,
+  'title': string,
+  'author': string,
+  'discription': string
+}
