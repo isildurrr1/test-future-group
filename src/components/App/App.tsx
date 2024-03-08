6281d5eb718e5d7e91ac31de2b7d7fa74b687edd
Andrey Romashev <andreygaranin.0@yandex.ru>
2024-03-08 16:57:31 +0700

    add .env

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -9,7 +9,6 @@ import Loader from '../Loader/Loader';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 
 const App = () => {
-
   const navigate = useNavigate();
   const [totalBooks, setTotalBooks] = useState<number>();
   const [selectedBook, setSelectedBook] = useState<IBook>();
@@ -25,59 +24,55 @@ const App = () => {
     setStartBookIndex(0);
     navigate('/books')
   }
+
   const handleBookClick = (book: IBook) => {
     setSelectedBook(book);
     navigate('/selected-book')
   }
+
   const handleLoadMoreClick = () => setStartBookIndex(startBookIndex + 30);
 
   const BASE_URL = 'https://www.googleapis.com/books/v1/';
 
-  // useEffect(() => {
-  //   const handleBackButton = () => {
-  //     // Здесь вы можете выполнить необходимые действия
-  //     // когда пользователь нажимает кнопку "назад" в браузере.
-  //     console.log('Нажата кнопка "назад"');
-  //   };
-
-  //   window.addEventListener('popstate', handleBackButton);
-
-  //   return () => {
-  //     window.removeEventListener('popstate', handleBackButton);
-  //   };
-  // }, []);
-
   useEffect(() => {
     if (searchOption) {
-      setLoading(true)
-      fetch(`${BASE_URL}volumes?q=${searchOption.searchText}:${searchOption.category}&orderBy=${searchOption.sort}&maxResults=30&startIndex=${startBookIndex}`, {
-        headers: {
-          'Authorization': 'key=AIzaSyC9Ujvz5Ff7wxHAm8IPmU0xf634nieW8Qk',
-        },
-      })
-        .then(res => res.json())
-        .then((data) => {
-          let array: IBook[] = []
-          data.items.forEach((item: any) => {
-            const newObj: IBook = {
-              'category': item.volumeInfo.categories?.join(', '),
-              'smallThumbnail': item.volumeInfo.imageLinks?.smallThumbnail,
-              'thumbnail': item.volumeInfo.imageLinks?.thumbnail,
-              'title': item.volumeInfo.title,
-              'author': item.volumeInfo.authors?.join(', '),
-              'discription': item.volumeInfo.description,
-            };
-            array.push(newObj);
-          })
-          if (!startBookIndex) {
-            setTotalBooks(data.totalItems);
-            setBooksData(array);
-          } else {
-            setBooksData(prevBooksData => [...prevBooksData, ...array]);
-          }
-          setLoading(false)
+      setLoading(true);
+      const timer = setTimeout(() => {
+        fetch(`${BASE_URL}volumes?q=${searchOption.searchText}:${searchOption.category}&orderBy=${searchOption.sort}&maxResults=30&startIndex=${startBookIndex}`, {
+          headers: {
+            'Authorization': `key=${process.env.REACT_APP_API_KEY}`,
+          },
         })
-        .catch(error => console.error('Ошибка:', error));
+          .then(res => res.json())
+          .then((data) => {
+            let array: IBook[] = [];
+            data.items.forEach((item: any) => {
+              const newObj: IBook = {
+                'category': item.volumeInfo.categories?.join(', '),
+                'smallThumbnail': item.volumeInfo.imageLinks?.smallThumbnail,
+                'thumbnail': item.volumeInfo.imageLinks?.thumbnail,
+                'title': item.volumeInfo.title,
+                'author': item.volumeInfo.authors?.join(', '),
+                'discription': item.volumeInfo.description,
+              };
+              array.push(newObj);
+            });
+
+            if (startBookIndex === 0) {
+              setTotalBooks(data.totalItems);
+              setBooksData(array);
+            } else {
+              setBooksData(prevBooksData => [...prevBooksData, ...array]);
+            }
+
+            setLoading(false);
+          })
+          .catch(error => {
+            console.error('Ошибка:', error);
+            setLoading(false);
+          });
+      }, 500);
+      return () => clearTimeout(timer);
     }
   }, [searchOption, startBookIndex]);
 
@@ -88,7 +83,7 @@ const App = () => {
         <Route path='/' element={<></>} />
         <Route path='/books' element={
           <>
-            < CardsContainer
+            <CardsContainer
               onBookClick={handleBookClick}
               books={booksData}
               totalBooks={totalBooks}
